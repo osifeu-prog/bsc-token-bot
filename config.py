4bef6d6b71bea19babaac418d8f8cbb3b8f35aa0
Osif ungar <osif.e.u@gmail.com>
2025-10-27 16:10:23 +0200

    Update and rename .env to config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+OWNER_WALLET_ADDRESS = os.getenv("OWNER_WALLET_ADDRESS")
+OWNER_WALLET_PRIVATE_KEY = os.getenv("OWNER_WALLET_PRIVATE_KEY")
+TOKEN_CONTRACT_ADDRESS = os.getenv("TOKEN_CONTRACT_ADDRESS")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+BSC_RPC_URL = os.getenv("BSC_RPC_URL")
+CHAIN_ID = int(os.getenv("CHAIN_ID", 56))
+SYMBOL = os.getenv("SYMBOL", "SLH")
