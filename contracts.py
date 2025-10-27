60ca506414f10c41eac45c6cb6a4978d9e26b0da
Osif ungar <osif.e.u@gmail.com>
2025-10-27 16:22:20 +0200

    Update and rename history.js to contracts.py

---
+++ contracts.py
@@ -0,0 +1,19 @@
+token_abi = [
+    {
+        "constant": True,
+        "inputs": [{"name": "owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function"
+    },
+    {
+        "constant": False,
+        "inputs": [
+            {"name": "to", "type": "address"},
+            {"name": "value", "type": "uint256"}
+        ],
+        "name": "transfer",
+        "outputs": [{"name": "success", "type": "bool"}],
+        "type": "function"
+    }
+]
