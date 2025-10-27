53162c0885e713e60622c38488578eaa5ba6a731
Osif ungar <osif.e.u@gmail.com>
2025-10-27 16:33:57 +0200

    Update users.py

--- users.py
+++ users.py
@@ -1,21 +1,36 @@
-# מאגר זיכרון פשוט בזמן ריצה. לשמירה פרקטית, החלף ב‑DB
-users = {}
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+from typing import Optional
+from config import DATABASE_URL
 
-def register_user(user_id, wallet_address=None):
-    if user_id not in users:
-        users[user_id] = {
-            "wallet": wallet_address,
-            "store": []
-        }
-    else:
+# מודל משתמש פשוט
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    telegram_id: int
+    wallet_address: Optional[str] = None
+
+engine = create_engine(DATABASE_URL, echo=False)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_user_by_telegram(telegram_id: int):
+    with Session(engine) as s:
+        stmt = select(User).where(User.telegram_id == telegram_id)
+        res = s.exec(stmt).first()
+        return res
+
+def create_or_update_user(telegram_id: int, wallet_address: str = None):
+    with Session(engine) as s:
+        user = get_user_by_telegram(telegram_id)
+        if not user:
+            user = User(telegram_id=telegram_id, wallet_address=wallet_address)
+            s.add(user)
+            s.commit()
+            s.refresh(user)
+            return user
         if wallet_address:
-            users[user_id]["wallet"] = wallet_address
-    return users[user_id]
-
-def get_user(user_id):
-    return users.get(user_id)
-
-def set_wallet(user_id, wallet_address):
-    register_user(user_id, wallet_address)
-    users[user_id]["wallet"] = wallet_address
-    return users[user_id]
+            user.wallet_address = wallet_address
+            s.add(user)
+            s.commit()
+            s.refresh(user)
+        return user
